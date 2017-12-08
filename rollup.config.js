6d6095450a2553ec42a6a470f910f4ceb88183a2
Chris Alderson <ChrisAlderson@protonmail.com>
2017-12-09 00:43:49 +0100

    feat: Build with Rollup to reduce package size (#19)
    * feat: Build with Rollup to reduce package size
    
    * chore: Use uglify to minify the code
    
    * fix(build): Fix warning with url as external

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import uglify from 'rollup-plugin-uglify'
+import { minify } from 'uglify-es'
+import {
+  main,
+  module,
+  dependencies
+} from './package.json'
+
+export default {
+  input: './src/index.js',
+  external: [
+    ...Object.keys(dependencies),
+    'child_process',
+    'cluster',
+    'fs',
+    'http',
+    'os',
+    'path',
+    'url'
+  ],
+  plugins: [
+    resolve(),
+    babel(),
+    uglify({}, minify)
+  ],
+  output: [{
+    file: main,
+    format: 'cjs'
+  }, {
+    file: module,
+    format: 'es'
+  }]
+}
